8e6d52fb1753be2408f2c89c41b527e24048b916
Reid Vandewiele <reid@puppet.com>
2020-01-17 10:19:45 -0800

    Rework algorithm to read hiera.yaml from master
    So that if the default hiera.yaml changes, the logic will still work,
    and the source of truth for the contents of hiera.yaml in a deployment
    managed by peadm is not the peadm module itself.

---
+++ tasks/read_file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+main() {
+  cat "$PT_path"
+}
+
+outfile=$(mktemp)
+main "$@" >"$outfile" 2>&1
+exit_code=$?
+
+cat <<EOS
+  {
+    "content": $(python -c "import json; print json.dumps(open('$outfile','r').read())"),
+    "exit-code": $?
+  }
+EOS
+
+rm "$outfile"
+exit $exit_code
